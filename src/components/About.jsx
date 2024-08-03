ebb7116ad3e4e58c534d9201d8724731740cec0f
Mr0Hadi <hadibalaei.dev7@gmail.com>
2024-08-04 01:48:50 +0330

    Writing About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from 'react';
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
 
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion'
+
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          className="rounded-[20px] bg-tertiary py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+        >
+          <img src={icon} alt={title} className="object-contain w-16 h-16" />
+          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+}
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.dev variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.dev>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm hadi, very khaste. zendany dar shahed,
+      </motion.p>
+
+      <div className="flex flex-wrap gap-10 mt-20">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
 
 export default About
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,16 +25,16 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
-      <div className="absolute flex items-center justify-center w-full xs:bottom-10 bottom-32">
+      <div className="absolute flex items-center justify-center w-full xs:bottom-4 bottom-15">
         <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+          <div className="w-[30px] h-[50px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.dev
               animate={{ y: [0, 24, 0] }}
               transition={{
                 duration: 1.5, repeat: Infinity,
                 repeatType: 'loop'
               }}
-              className="w-3 h-3 mb-1 rounded-full bg-secondary" />
+              className="w-5 h-1.5 mb-1 rounded-full bg-secondary" />
           </div>
         </a>
       </div>
