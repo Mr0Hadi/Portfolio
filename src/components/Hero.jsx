df4512cb61a3041684e696ba8eaee7c2e8330ac9
Mr0Hadi <hadibalaei.dev7@gmail.com>
2024-08-03 20:10:59 +0330

    Writing computer components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={`${styles.paddingx} absolute inset-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}
+        className={`${styles.paddingx} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col items-center justify-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
@@ -18,7 +18,7 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className="text-[#915eff]">Hadi</span>
           </h1>
-          <p className={`${styles.heroSubText}mt-2 text-white-100`}>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop 3D visuals, user <br className="hidden sm:block" />
             interface and web applications
           </p>
